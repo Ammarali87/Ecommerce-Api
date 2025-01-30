bcad011aa768d432cc2c9169fae8bcf705eddb2e
Ammarali87 <amarhero501@gmail.com>
2025-01-30 18:55:13 +0200

    tt

--- my-app/app.js
+++ my-app/app.js
@@ -2,18 +2,14 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import mongoose from 'mongoose'
-import path from 'path'
 // import { productRouter } from './routs/prodcutRoute' // Corrected import name
-// import { CartModel } from './moduls/cartModel'
-// import { seedRouter } from './routs/seedRoute'
-// import { userRouter } from './routs/userRoute'
-// import { ProductModel } from './moduls/productModel'
 
 dotenv.config()
 
-const MONGODB_URI ="mongodb+srv://amar:Kofta@store.abadx.mongodb.net/?retryWrites=true&w=majority&appName=Store"
-//   process.env.MONGODB_URI || 'mongodb://localhost/tsmernamazonadb'
-mongoose.set('strictQuery', true)
+const MONGODB_URI = "mongodb+srv://amar:Kofta@store.abadx.mongodb.net/?retryWrites=true&w=majority&appName=Store"
+  // process.env.MONGODB_URI || 'mongodb://localhost/ecommerce'
+
+  mongoose.set('strictQuery', true)
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
@@ -24,25 +20,12 @@ mongoose
   })
 
 const app = express()
-app.use(
-  cors({
-    credentials: true,
-    origin: ['http://localhost:5173'], // Make sure this matches your frontend URL
-  })
-)
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // app.use('/api/products', productRouter)
-// app.use('/api/users', userRouter)
-// app.use('/api/seed', seedRouter)
 
 
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join
-//     (__dirname, './public')) // Make sure index.html is being served
-// )
 
 app.get('/', (req, res) => {
   res.send("Hello World")
