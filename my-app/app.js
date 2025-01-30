046c1392c562a7eae2aa0df402425ed5d0b3293b
Ammarali87 <amarhero501@gmail.com>
2025-01-30 22:24:29 +0200

    tt

--- my-app/app.js
+++ my-app/app.js
@@ -2,7 +2,8 @@ import dotenv from 'dotenv'
 import express from 'express'
 import mongoose from 'mongoose'
 import morgan from 'morgan'
-// import { productRouter } from './routs/prodcutRoute' // Corrected import name
+import { User } from "./models/userModel";
+
 
 dotenv.config()
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/ecommerce'
@@ -23,16 +24,59 @@ app.use(express.urlencoded({ extended: true }))
 
 
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+  console.log("morgan enable in dev")
+}
 app.get('/', (req, res) => {
   res.send("Hello World")
 })
 
 
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
-  console.log("morgan enable in dev")
-}
+
+
+
+
+
+
+// make fun use async await only in create new no need 
+// use try catch 
+
+const createUsers = async () => {
+  try {
+    // // First method: Using new and save()
+    // const userNew = new User({ 
+    //   name: "Ali",
+    //   age: 25
+    // });
+    // await userNew.save();
+    // console.log('User created using save():', userNew);
+
+    // Second method: Using create()
+    const userNew_ = await User.create({ 
+      name: "Ahmed",
+      age: 30
+    });
+    console.log('User created using create():', userNew_);
+
+  } catch (error) {
+    console.error('Error creating users:', error);
+  }
+};
+
+// Execute the function
+createUsers();
+
+
+
+
+
+
+
+
+
+
 
 const PORT = process.env.PORT || '4000'
 app.listen(PORT, () => {
