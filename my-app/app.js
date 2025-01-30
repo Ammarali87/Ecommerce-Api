0abe19c5a3e84b6325e6a8c2de262c93b1162ce4
Ammarali87 <amarhero501@gmail.com>
2025-01-30 21:19:38 +0200

    e

--- my-app/app.js
+++ my-app/app.js
@@ -1,14 +1,13 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import mongoose from 'mongoose'
+import morgan from 'morgan'
 // import { productRouter } from './routs/prodcutRoute' // Corrected import name
 
 dotenv.config()
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/ecommerce'
 
-const MONGODB_URI = "mongodb+srv://amar:Kofta@store.abadx.mongodb.net/?retryWrites=true&w=majority&appName=Store"
-  // process.env.MONGODB_URI || 'mongodb://localhost/ecommerce'
-
-  mongoose.set('strictQuery', true)
+mongoose.set('strictQuery', true)
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
@@ -17,12 +16,10 @@ mongoose
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error) // More specific error logging
   })
-
-const app = express()
-app.use(express.json())
+  
+  const app = express()
+  app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-// app.use('/api/products', productRouter)
 
 
 
@@ -30,14 +27,14 @@ app.get('/', (req, res) => {
   res.send("Hello World")
 })
 
-app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
-})
 
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+  console.log("morgan enable in dev")
+}
 
 const PORT = process.env.PORT || '4000'
-
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`)
 })
