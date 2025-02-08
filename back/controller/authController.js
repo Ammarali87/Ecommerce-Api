528aaf394c14f36cc782bac41af4c5f991e7e284
Amar Ibrahim <113317548+Ammarali87@users.noreply.github.com>
2025-02-08 22:23:44 +0200

    Update authController.js
    rerrrr

--- back/controller/authController.js
+++ back/controller/authController.js
@@ -1,27 +1,31 @@
 import {User} from '../models/userModel.js';
 import jwt  from 'jsonwebtoken';
-
+ 
 // Function to generate JWT
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-};
+ 
+// expiresIn:"24h"
+// ValidationError
 
+
+  
 // **Signup**
 export async function signup(req, res) {
   try {
     const { name, email, password } = req.body;
     const newUser = await User.create({ name, email, password });
-
+   
     const token = signToken(newUser._id);
     console.log("sign up success")
     res.status(201).json({ status: 'success', token, user: { id: newUser._id, name, email } });
-  } catch (err) {  
-    if (err. name === 'ValidationError') {
+  } catch (err) { 
+      if (err. name === 'ValidationError') {
       // Object.valuesExtract all error messages: 
       // If multiple fields are invalid, it returns an array
       const messages = Object.values(err.errors).map((er) => er.message);
       return res.status(400).json({ status: 'fail', message: messages });
-    }
+    } // const messes = Objec.valus(errObj.errorsArr).map((er)=> err.mesage
     res.status(400).json({ status: 'fail', message: err.message });
   }
 }
