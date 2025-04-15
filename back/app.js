2097a4c152c5cfd5e3b64c3b64cdf263022d2c68
AmmarAli87 <amarhero501@gmail.com>
2025-04-15 09:46:12 +0200

    s

--- back/app.js
+++ back/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import morgan from 'morgan';
+import morgan from 'morgan'; 
 import cors from "cors";
 import compression from 'compression';
 import helmet from 'helmet';
@@ -12,7 +12,7 @@ import cookieParser from "cookie-parser";
 import csrf from "csurf"; // Add this import
 
 import errorMiddleware from './middleware/errorMiddleware.js';
-import { connect } from './config/mongo.ts';
+import { connect } from './config/mongo.js';
 import authRoutes from './routes/authRoute.js';
 import storeRoutes from './routes/store.js';
 import brandRoute from './routes/brandRoute.js';
